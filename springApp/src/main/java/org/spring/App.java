9307b6e010cb31ea653efacfdf3fca9edff27d07
ram kumar <rr697144@gmail.com>
2025-04-12 11:31:58 +0530

    collection dependency injection using setter method

--- springApp/src/main/java/org/spring/App.java
+++ springApp/src/main/java/org/spring/App.java
@@ -1,6 +1,6 @@
 package org.spring;
 
-import org.spring.primitive.Student;
+import org.spring.collection.Student;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -18,10 +18,13 @@ public class App
         System.out.println(ap);
 
         Student st1 = ap.getBean("st1", Student.class);
-        Student st2 = ap.getBean("st2", Student.class);
-
         System.out.println(st1.toString());
-        System.out.println(st2.toString());
+
+//        Student st1 = ap.getBean("st1", Student.class);
+//        Student st2 = ap.getBean("st2", Student.class);
+//
+//        System.out.println(st1.toString());
+//        System.out.println(st2.toString());
 
     }
 }
---
+++ springApp/src/main/java/org/spring/collection/Student.java
@@ -0,0 +1,66 @@
+package org.spring.collection;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Student {
+
+    private Long id;
+    private String name;
+    private List<String>  address;
+
+    private Set<String> phone;
+    private Map<String,String> course;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getAddress() {
+        return address;
+    }
+
+    public void setAddress(List<String> address) {
+        this.address = address;
+    }
+
+    public Set<String> getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Set<String> phone) {
+        this.phone = phone;
+    }
+
+    public Map<String, String> getCourse() {
+        return course;
+    }
+
+    public void setCourse(Map<String, String> course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                ", phone=" + phone +
+                ", course=" + course +
+                '}';
+    }
+}
