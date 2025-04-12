f8453e2eff82778ec179967907aea7a6e38f3137
ram kumar <rr697144@gmail.com>
2025-04-12 13:22:53 +0530

    interface ke use karke lifecycle hai

--- springlifecycle/src/main/java/org/spring/App.java
+++ springlifecycle/src/main/java/org/spring/App.java
@@ -1,6 +1,6 @@
 package org.spring;
 
-import org.spring.xml.Student;
+import org.spring.impl.Student;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -16,8 +16,12 @@ public class App
         System.out.println( "Hello World!" );
         AbstractApplicationContext ap = new ClassPathXmlApplicationContext("Beans.xml");
         System.out.println(ap);
-        Student st = ap.getBean("st1", Student.class);
-        System.out.println(st.toString());
+
+        Student student = ap.getBean("st1",Student.class);
+        System.out.println(student.toString());
         ap.registerShutdownHook();
+//        Student st = ap.getBean("st1", Student.class);
+//        System.out.println(st.toString());
+//        ap.registerShutdownHook();
     }
 }
