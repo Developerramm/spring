edd942a17d81a50f1a7b4ffa4f7b511fd31997cc
ram kumar <rr697144@gmail.com>
2025-04-16 13:20:37 +0530

    fetch data from database using jdbc

--- JDBC_C1/src/FetchStudent.java
+++ JDBC_C1/src/FetchStudent.java
@@ -1,6 +1,36 @@
+import java.sql.*;
+
 public class FetchStudent {
 
     public static void main(String[] args) {
-        
+        String url = "jdbc:mysql://localhost:3306/spark";
+        String username = "root";
+        String password = "ramkumar";
+
+        String query = "SELECT * FROM spark.students;";
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url,username,password);
+            Statement stmt = conn.createStatement();
+            ResultSet result = stmt.executeQuery(query);
+
+            while(result.next()){
+                int id = result.getInt("id");
+                String name = result.getString("name");
+                int age = result.getInt("age");
+                String course = result.getString("course");
+
+                System.out.println(id + " " + name + " " + age + " " + course);
+            }
+            result.close();
+            stmt.close();
+            conn.close();
+            System.out.println("Connection created successfully");
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
